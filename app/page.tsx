a6b98756179e3672dcac623d65869eff8d42ea9a
Jon Willis <jondwillis@gmail.com>
2023-04-13 23:03:30 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // ✅ This pattern works. You can pass a Server Component
 // as a child or prop of a Client Component.
 import ClientComponent from "./client";
+import ClientStats from "./ClientStats";
 import Stats from "./Stats";
 
 // Pages are Server Components by default
@@ -8,6 +9,7 @@ export default function Page() {
   return (
     <ClientComponent>
       <Stats clientId={''}/>
+      <ClientStats/>
     </ClientComponent>
   );
 }
